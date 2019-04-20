1be84f33502cdc59887fbf6b665e05a8bb4bd49b
Fellowship Student <hackbright@hackbrightacademy.com>
2019-04-20 13:01:08 -0700

    Getting the Player's Name

--- game.py
+++ game.py
@@ -3,22 +3,6 @@
 # Put your code here
 import random
 name = input("Howdy, what's your name? >")
-print(name)
-print(name + ", " "I'm thinking of a number between 1 and 100. Try to guess my number.")
 
-answer = (random.randint(0,100))
-attempts = 0
-guess = -1
-
-while answer != guess:
-	guess = int(input("What's your guess?"))
-	if guess > answer:
-		print("Your guess is too high, try again.")
-		attempts = attempts + 1
-	elif guess < answer: 
-		print("Your guess is too low, try again")
-		attempts = attempts + 1
-	elif guess == answer:
-		print("Well done, " + name + " You found my number in " + str(attempts) + " tries!")
 
 
